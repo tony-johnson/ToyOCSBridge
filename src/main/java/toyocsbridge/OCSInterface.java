dea55c489c1da7fd4388da9247ed30fce0c7ad30
Tony Johnson <tonyj@stanford.edu>
2016-06-02 15:13:08 -0700

    Gracefully fallback to standalone mode if OCS is not configured.

--- src/main/java/toyocsbridge/OCSInterface.java
+++ src/main/java/toyocsbridge/OCSInterface.java
@@ -46,7 +46,12 @@ public class OCSInterface {
 
             @Override
             public void run() {
-                ocsInterface.run();
+                try {
+                    ocsInterface.run();
+                } catch (Exception x) {
+                    logger.log(Level.WARNING, "Failed to initialize OCS communication layer, "
+                            + "check that OCS has been setup correctly. Reverting to standalone mode.", x);
+                }
             }
 
         };
@@ -55,71 +60,75 @@ public class OCSInterface {
     }
 
     void run() {
-        runThread = Thread.currentThread();
-        // Currently we have to poll for each command
-        mgr.salProcessor("camera_command_setFilter");
-        camera.command_setFilter setFilterCommand = new camera.command_setFilter();
-        mgr.salProcessor("camera_command_takeImages");
-        camera.command_takeImages takeImagesCommand = new camera.command_takeImages();
-        mgr.salProcessor("camera_command_initImage");
-        camera.command_initImage initImageCommand = new camera.command_initImage();
-        mgr.salProcessor("camera_command_enable");
-        camera.command_enable enableCommand = new camera.command_enable();
-        mgr.salProcessor("camera_command_disable");
-        camera.command_disable disableCommand = new camera.command_disable();
-        mgr.salProcessor("camera_command_enterControl");
-        camera.command_enterControl enterControlCommand = new camera.command_enterControl();
-        mgr.salProcessor("camera_command_exit");
-        camera.command_exit exitCommand = new camera.command_exit();
-        mgr.salProcessor("camera_command_start");
-        camera.command_start startCommand = new camera.command_start();       
-        mgr.salProcessor("camera_command_standby");
-        camera.command_standby standbyCommand = new camera.command_standby();         
-        
-        while (!shutdown) {
-            int cmdId = mgr.acceptCommand_setFilter(setFilterCommand);
-            if (cmdId > 0) {
-                bridge.setFilter(cmdId, setFilterCommand.name);
+        try {
+            runThread = Thread.currentThread();
+            // Currently we have to poll for each command
+            mgr.salProcessor("camera_command_setFilter");
+            camera.command_setFilter setFilterCommand = new camera.command_setFilter();
+            mgr.salProcessor("camera_command_takeImages");
+            camera.command_takeImages takeImagesCommand = new camera.command_takeImages();
+            mgr.salProcessor("camera_command_initImage");
+            camera.command_initImage initImageCommand = new camera.command_initImage();
+            mgr.salProcessor("camera_command_enable");
+            camera.command_enable enableCommand = new camera.command_enable();
+            mgr.salProcessor("camera_command_disable");
+            camera.command_disable disableCommand = new camera.command_disable();
+            mgr.salProcessor("camera_command_enterControl");
+            camera.command_enterControl enterControlCommand = new camera.command_enterControl();
+            mgr.salProcessor("camera_command_exit");
+            camera.command_exit exitCommand = new camera.command_exit();
+            mgr.salProcessor("camera_command_start");
+            camera.command_start startCommand = new camera.command_start();
+            mgr.salProcessor("camera_command_standby");
+            camera.command_standby standbyCommand = new camera.command_standby();
+
+            while (!shutdown) {
+                int cmdId = mgr.acceptCommand_setFilter(setFilterCommand);
+                if (cmdId > 0) {
+                    bridge.setFilter(cmdId, setFilterCommand.name);
+                }
+                cmdId = mgr.acceptCommand_takeImages(takeImagesCommand);
+                if (cmdId > 0) {
+                    bridge.takeImages(cmdId, takeImagesCommand.expTime, takeImagesCommand.numImages, takeImagesCommand.shutter);
+                }
+                cmdId = mgr.acceptCommand_initImage(initImageCommand);
+                if (cmdId > 0) {
+                    bridge.initImage(cmdId, initImageCommand.deltaT);
+                }
+                cmdId = mgr.acceptCommand_enable(enableCommand);
+                if (cmdId > 0) {
+                    bridge.enable(cmdId);
+                }
+                cmdId = mgr.acceptCommand_disable(disableCommand);
+                if (cmdId > 0) {
+                    bridge.disable(cmdId);
+                }
+                cmdId = mgr.acceptCommand_enterControl(enterControlCommand);
+                if (cmdId > 0) {
+                    bridge.enterControl(cmdId);
+                }
+                cmdId = mgr.acceptCommand_exit(exitCommand);
+                if (cmdId > 0) {
+                    bridge.exit(cmdId);
+                }
+                cmdId = mgr.acceptCommand_start(startCommand);
+                if (cmdId > 0) {
+                    bridge.start(cmdId, startCommand.configuration);
+                }
+                cmdId = mgr.acceptCommand_standby(standbyCommand);
+                if (cmdId > 0) {
+                    bridge.standby(cmdId);
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    logger.log(Level.SEVERE, "Unexpected interruption", ex);
+                }
             }
-            cmdId = mgr.acceptCommand_takeImages(takeImagesCommand);
-            if (cmdId > 0) {
-                bridge.takeImages(cmdId, takeImagesCommand.expTime, takeImagesCommand.numImages, takeImagesCommand.shutter);
-            }
-            cmdId = mgr.acceptCommand_initImage(initImageCommand);
-            if (cmdId > 0) {
-                bridge.initImage(cmdId, initImageCommand.deltaT);
-            }
-            cmdId = mgr.acceptCommand_enable(enableCommand);
-            if (cmdId > 0) {
-                bridge.enable(cmdId);
-            }
-            cmdId = mgr.acceptCommand_disable(disableCommand);
-            if (cmdId > 0) {
-                bridge.disable(cmdId);
-            }
-            cmdId = mgr.acceptCommand_enterControl(enterControlCommand);
-            if (cmdId > 0) {
-                bridge.enterControl(cmdId);
-            }
-            cmdId = mgr.acceptCommand_exit(exitCommand);
-            if (cmdId > 0) {
-                bridge.exit(cmdId);
-            }
-            cmdId = mgr.acceptCommand_start(startCommand);
-            if (cmdId > 0) {
-                bridge.start(cmdId,startCommand.configuration);
-            }
-            cmdId = mgr.acceptCommand_standby(standbyCommand);
-            if (cmdId > 0) {
-                bridge.standby(cmdId);
-            }
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ex) {
-                logger.log(Level.SEVERE, "Unexpected interruption", ex);
-            }
+            mgr.salShutdown();
+        } finally {
+            runThread = null;
         }
-        mgr.salShutdown();
     }
 
     void shutdown() throws InterruptedException {
@@ -136,7 +145,7 @@ public class OCSInterface {
         @Override
         protected void reportComplete(OCSCommand command) {
             super.reportComplete(command);
-            if (command.getCmdId() != 0) {
+            if (command.getCmdId() != 0 && runThread != null) {
                 mgr.ackCommand_takeImages(command.getCmdId(), SAL_camera.SAL__CMD_COMPLETE, 0, "Done : OK");
             }
 
@@ -145,7 +154,7 @@ public class OCSInterface {
         @Override
         protected void reportError(OCSCommand command, Exception ex) {
             super.reportError(command, ex);
-            if (command.getCmdId() != 0) {
+            if (command.getCmdId() != 0 && runThread != null) {
                 mgr.ackCommand_takeImages(command.getCmdId(), SAL_camera.SAL__CMD_FAILED, 0, "Error : " + ex.getMessage());
             }
         }
@@ -153,7 +162,7 @@ public class OCSInterface {
         @Override
         protected void acknowledgeCommand(OCSCommand command, Duration timeout) {
             super.acknowledgeCommand(command, timeout);
-            if (command.getCmdId() != 0) {
+            if (command.getCmdId() != 0 && runThread != null) {
                 mgr.ackCommand_takeImages(command.getCmdId(), SAL_camera.SAL__CMD_INPROGRESS, (int) timeout.getSeconds(), "Ack : OK");
             }
         }
@@ -161,7 +170,7 @@ public class OCSInterface {
         @Override
         protected void rejectCommand(OCSCommand command, String reason) {
             super.rejectCommand(command, reason);
-            if (command.getCmdId() != 0) {
+            if (command.getCmdId() != 0 && runThread != null) {
                 mgr.ackCommand_takeImages(command.getCmdId(), SAL_camera.SAL__CMD_NOACK, 0, "Ack : NO");
             }
         }
